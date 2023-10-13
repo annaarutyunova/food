56b36ad3e485fce71a125845fdf2a4c4cc7f56de
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-10-13 10:38:57 -0600

    delete space

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,6 +3,6 @@ const routes = express.Router();
 const base = require('../controllers/baseController.js');
 
 routes.get('/', base.listRecipes)
-routes.post('/' ,base.addPlace)
+routes.post('/', base.addPlace)
 
 module.exports = routes;
