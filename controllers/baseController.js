2d98c30d621b062fae43b01dd429fe2180bc0f11
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-10-14 18:15:17 -0600

    add routes

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,5 +1,5 @@
 const mongodb = require('../model/index.js');
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const listRecipes = async (req,res)  => {
     const recipes = await mongodb.getDb().db('food').collection('restaurants').find();
@@ -33,5 +33,39 @@ const getById = async (req, res) => {
     });
 };
 
+const updatePlace = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    // be aware of updateOne if you only want to update specific fields
+    const body = {
+        name: req.body.name,
+        cuisine: req.body.cuisine,
+        address: req.body.address,
+        rating: req.body.rating,
+        hours: req.body.hours
+    };
+    const response = await mongodb
+      .getDb()
+      .db('food')
+      .collection('restaurants')
+      .replaceOne({ _id: userId }, body);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the restaurant.');
+    }
+};
 
-module.exports = {listRecipes, addPlace, getById};
+const deletePlace = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db('food').collection('restaurants').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+
+module.exports = {listRecipes, addPlace, getById, updatePlace,deletePlace};
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,5 +5,7 @@ const base = require('../controllers/baseController.js');
 routes.get('/', base.listRecipes)
 routes.post('/', base.addPlace)
 routes.get('/:id', base.getById)
+routes.put('/:id', base.updatePlace)
+routes.delete('/:id', base.deletePlace)
 
 module.exports = routes;
