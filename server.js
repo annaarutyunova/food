4055749bbf9db82b925b4184636f24d011b5cea8
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-11-02 15:16:24 -0600

    auth login route

--- server.js
+++ server.js
@@ -7,24 +7,31 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const cors = require('cors');
 /////These lines are new
-const { auth } = require('express-oauth2-jwt-bearer');
-const jwtCheck = auth({
+const { auth: auth1} = require('express-oauth2-jwt-bearer');
+const { auth: auth2 } = require("express-openid-connect");
+const jwtCheck = auth1({
   audience: 'https://food-lrc1.onrender.com/',
   issuerBaseURL: 'https://dev-us5m3466ym0sk11q.us.auth0.com/',
   tokenSigningAlg: 'RS256'
 });
-////////// The end of new line
 
-// const { signupValidation, loginValidation } = require('./validation.js');
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth2(config));
+
+////////// The end of new lines
 
 app.use('/api-docs', jwtCheck, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json())
 app.use('/', require('./routes'))
 app.use(cors());
-
-// app.listen(process.env.PORT || 8080, () => {
-//     console.log('Web Server is listening at port ' + (process.env.PORT || 8080));
-// });
 
 mongodb.initDb((err) => {
     if (err) {
